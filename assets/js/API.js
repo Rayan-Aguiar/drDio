621261091b9098b92f4b6cfd35a61bc49213ac28
Rayan <rayansiqueira@gmail.com>
2023-11-08 17:02:56 -0300

    blogs

--- assets/js/API.js
+++ assets/js/API.js
@@ -1,15 +1,15 @@
 const coursesCardsContainer = document.getElementById("courses-cards");
-
-
-
+const blogCardsContainer = document.getElementById("blogs-Card");
 
 async function fetchCourses() {
   try {
-    const response = await fetch("https://hudson.tutorcasts.app/api/iepem");
+    const response = await fetch("https://rayandev.tutorcasts.app/api/iepem");
+    
     if (!response.ok) {
       throw new Error(`Erro na solicitação: ${response.status}`);
     }
     const data = await response.json();
+    console.log(data);
     if (data.hasOwnProperty("courses") && Array.isArray(data.courses)) {
       return data.courses;
     } else {
@@ -22,12 +22,14 @@ async function fetchCourses() {
 }
 
 function formatPriceToBRL(price) {
-    if(price === 0){
-        return "Curso gratuito";
-    }else{
-        return price.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
-
-    }
+  if (price === 0) {
+    return "Curso gratuito";
+  } else {
+    return price.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  }
 }
 
 function createCoursesCard(course) {
@@ -49,7 +51,7 @@ function createCoursesCard(course) {
                         </div>
                         
                         <div class="courses-content">
-                            <a href="https://iepem.g1learn.app/courses" target="_blank"><button class="btn btn-outline-primary">Saiba mais</button></a>
+                            <a href="https://iepem.g1learn.app/courses" target="_blank"><button class="st-btn st-style2 st-color1 st-size-medium">Saiba mais</button></a>
                         </div>
                         
                         <div class="d-none">${course.id}</div>
@@ -71,3 +73,46 @@ async function renderCoursesCards() {
 }
 
 renderCoursesCards();
+
+function createBlogCard(blog) {
+  const blogCard = document.createElement("div");
+  blogCard.className = "col-lg-4";
+
+  blogCard.innerHTML = `  
+  <div class="st-post st-style3">
+    <a href="blog-details-right-sidebar.html" class="st-post-thumb st-link-hover-wrap st-zoom">
+      <img class="st-zoom-in" src="assets/img/blog1.jpg" alt="blog1">
+      <span class="st-link-hover"><i class="fas fa-link"></i></span>
+    </a>
+    <div class="st-post-info">
+      <h2 class="st-post-title"><a href="blog-details-right-sidebar.html">Working in emergency medicine</a></h2>
+      <div class="st-post-meta">
+        <span class="st-post-date">Aug 21, 2020</span>
+        <span>
+          Posted by:<a href="#" class="st-post-avatar">
+            <span class="st-post-avatar-text"> James Lewis</span>
+          </a>
+        </span>
+      </div>
+      <div class="st-post-text">Lorem Ipsum is simply dummy text of the print ing and typesetting industry.
+        lorem Ipsum the industry's standard dummy text.</div>
+    </div>
+    <div class="st-post-footer">
+      <a href="blog-details-right-sidebar.html" class="st-btn st-style2 st-color1 st-size-medium">Read More</a>
+    </div>
+  </div>
+  <div class="st-height-b0 st-height-lg-b30"></div>  
+  `;
+  return blogCard;
+}
+
+async function renderBlogCard(){
+  const blogs = await fetchBlogs();
+
+  blogs.forEach((blog) => {
+    const blogCard = createBlogCard(blog);
+    blogCardContainer.appendChild(blogCard);
+  });
+}
+
+renderBlogCard();
